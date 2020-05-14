79c785f6ff94cb68484e6e9631f3d1907bc827ad
stephanemontoro <stephane.montoro@hotmail.com>
2020-05-14 15:12:25 +0200

    end

--- 3-controlflow+loopingwithRuby/session3.rb
+++ 3-controlflow+loopingwithRuby/session3.rb
@@ -4,42 +4,33 @@ puts 'Bonjour a tous !'
 
 # if, else, elsif
 
-heure = 12
+heure = 9
 
 if heure < 12
-  puts "C'est le matin"
+  puts "c'est le matin"
 elsif heure > 12
-  puts "C'est l'aprem"
+  puts "c'est l'aprem"
 else
-  puts "C'est midi"
-end
-
-# unless
-
-age = 17
-
-unless age < 18
- puts 'tu est majeur'
+  puts "c'est midi"
 end
 
 # Boolean operator
 
 # Operator boolean AND
+username = 'Pauline'
+password = 'toto'
 
-username = "pauline"
-password = "wagon"
-
-if username == "pauline" && password == "wagon"
-  puts 'tu est connecté'
+if username == 'Pauline' && password == 'wagon'
+  puts 'tu es connecté !'
 end
 
 # Operator boolean OR
 
-username = "pauline"
-password = "toto"
+username = 'Pauline'
+password = 'toto'
 
-if username == "pauline" || password == "wagon"
-  puts 'tu est connecté'
+if username == 'Pauline' || password == 'wagon'
+  puts 'tu est connecté !'
 end
 
 # loops & iterators
@@ -50,46 +41,65 @@ chiffre = 0
 
 while chiffre < 10
   puts chiffre
-  chiffre = chiffre + 1
+  chiffre = chiffre + 1 # chiffre += 1
 end
 
 # Until
 
 until chiffre > 10
   puts chiffre
-  chiffre = chiffre + 1
+  chiffre = chiffre + 1 # chiffre += 1
 end
 
 # Loop
 
 # For
 
-for number in 1..10
-  next if number.even?
+for number in 1...10
+  next if number.odd?
   puts number
 end
 
 # Iterators
 
 # Loop
-number = 0
+
+chiffre = 0
 
 loop do
-  number += 1
-  puts number
-  break if number > 5
+  puts chiffre
+  chiffre += 1
+  break if chiffre > 5
 end
+
 
 # Each
 
 lettres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 lettres.each do |lettre|
-  puts lettre.upcase
+  puts "voici la lettre : #{lettre} "
 end
+
+lettres.each { |lettre| puts "voici la lettre : #{lettre} " }
 
 # Times
 
-5.times do
- puts 'bonjour'
+25.times do
+  puts 'bonjour'
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
